9bc80750d126bc1a9a0c49c18893cdf86ecac423
alpakshbhagat <121705493+alpakshbhagat@users.noreply.github.com>
2024-02-27 15:02:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-function toggleClass() {
-    var menu = document.getElementById('menu');
-    menu.classList.toggle('active');
-    
+let toggleOne = document.querySelector(".nav-menu");
+let wrapper = document.querySelector(".wrapper");
+let counter = 0;
+function toggleAdder(){
+    if(counter%2==0){
+        toggleOne.classList.remove("nav-menu");
+        toggleOne.classList.add("toggle");
+        counter++;
+    }else{
+        toggleOne.classList.add("nav-menu");
+        toggleOne.classList.remove("toggle");
+        counter++;
     }
+}
+wrapper.addEventListener("click", toggleAdder);
